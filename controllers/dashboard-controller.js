8adab10e9f9dcf94c85b4fb840fdb8528edfe36d
Glitch (weathertop-weather-station) <none>
2024-08-05 11:38:51 +0000

    🍂🎯 Checkpoint ./controllers/dashboard-controller.js:68214783/193

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,9 +1,10 @@
-import {  }
+import { stationStore } from "../models/station-store.js";
 
 export const dashboardController = {
   async index(request, response) {
     const viewData = {
-      title: "Station 1 Dashboard",
+      title: "Station Dashboard",
+      stations: await stationStore.getAllStations(),
     };
     console.log("dashboard rendering");
     response.render("dashboard-view", viewData);
@@ -14,6 +15,7 @@ export const dashboardController = {
       title: request.body.title,
     };
     console.log(`adding station ${newStation.title}`);
+    await stationStore.addStation(newStation);
     response.redirect("/dashboard");
   },
   
