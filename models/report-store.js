0c179c92b26a67e6a6a1e43cae5f3b08546786b1
Glitch (weathertop-weather-station) <none>
2024-08-05 14:54:23 +0000

    🕴🍓 Checkpoint ./models/report-store.js:68214783/604

--- models/report-store.js
+++ models/report-store.js
@@ -19,7 +19,29 @@ export const reportStore = {
   },
   
   async getReportsByStationId(id) {
-    
+    await db.read();
+    return db.data.reports.filter((report) => report.stationid === id);
+  },
+  
+  async getReportById(id) {
+    await db.read();
+    return db.data.reports.find((report) => report._id === id);
+  },
+  
+  async deleteReport(id) {
+    await db.read();
+    const index = db.data.reports.findIndex((report) => report._id === id);
+    db.data.reports.splice(index, 1);
+    await db.write();
+  },
+  
+  async deleteAllReports() {
+    db.data.reports = [];
+    await db.write();
+  },
+  
+  async updateReport(report, updatedReport) {
+    report.title = updatedReport
   }
   
-}
+};
