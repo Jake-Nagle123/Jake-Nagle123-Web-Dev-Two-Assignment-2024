1154bfd25b388f8f1826e2a7a273f8c7ecbc4671
Glitch (weathertop-weather-station) <none>
2024-08-09 16:15:28 +0000

    💧🎄 Checkpoint ./controllers/station-controller.js:68214783/447

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -1,4 +1,5 @@
 import { stationStore } from "../models/station-store.js";
+import { reportStore } from "../models/report-store.js";
 
 export const stationController = {
   async index(request, response) {
@@ -9,4 +10,15 @@ export const stationController = {
     };
     response.render("station-view", viewData);
   },
+  
+  async addReport(request, repsonse) {
+    const station = await stationStore.getStationById(request.params.id);
+    const newReport = {
+      code: Number(request.body.code),
+      temperature: Number(request.body.temperature),
+      windspeed: Number(request.body.windspeed),
+      winddirection: Number(request.)
+    }
+  }
+  
 };
