53ec0e57c22d9c1d54f74881e366c5714a92c740
Glitch (weathertop-weather-station) <none>
2024-08-12 16:31:43 +0000

    📻🐲 Checkpoint ./routes.js:68214783/355

--- routes.js
+++ routes.js
@@ -6,12 +6,17 @@ import { accountsController } from "./controllers/accounts-controller.js";
 
 export const router = express.Router();
 
-router.get("/", dashboardController.index);
+router.get("/", accountsController.index);
+router.get("/login", accountsController.login);
+router.get("/signup", accountsController.signup);
+router.get("/logout", accountsController.logout);
+router.post("/register", accountsController.register);
+router.post("/authenticate", accountsController.authenticate);
+
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
 router.post("/dashboard/addstation", dashboardController.addStation);
 router.get("/station/:id", stationController.index);
 router.post("/station/:id/addreport", stationController.addReport);
 router.get("/dashboard/deletestation/:id", dashboardController.deleteStation);
-
 router.get("/station/:stationid/deletereport/:reportid", stationController.deleteReport);
