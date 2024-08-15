0076fa2fe892092146ced87b83fd845ee4d81714
Glitch (weathertop-weather-station) <none>
2024-08-15 15:36:21 +0000

    📡🍧 Checkpoint ./views/partials/add-station.hbs:68214783/8 ./models/user-store.js:68214783/12

--- models/user-store.js
+++ models/user-store.js
@@ -22,8 +22,8 @@ export const userStore = {
     return db.data.users.find((user) => user._id === id);
   },
   
-  async updateUser(id, updatedUser) {
-    const user = await this.getUserById(id);
+  async updateUser(userId, updatedUser) {
+    const user = await this.getUserById(userId);
     user.firstName = updatedUser.firstName;
     user.secondName = updatedUser.secondName;
     user.email = updatedUser.email;
