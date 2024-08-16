8dbc9c96d655671e5c2b2c8692a852e5007d9798
Glitch (weathertop-weather-station) <none>
2024-08-16 14:55:57 +0000

    ⛸⛩ Checkpoint ./controllers/station-controller.js:68214783/2

--- controllers/station-controller.js
+++ controllers/station-controller.js
@@ -21,7 +21,7 @@ export const stationController = {
     let maxTemp = null;
     if(station.reports.length > 0) {
       maxTemp = station.reports[0];
-      for(let i = 1; i > station.reports.length; i++) {
+      for(let i = 1; i < station.reports.length; i++) {
         if(station.reports[i].temperature > maxTemp.temperature) {
           maxTemp = station.reports[i];
         }
